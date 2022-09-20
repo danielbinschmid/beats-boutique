d9e1d760a327c7300ad1fa1a670300f15fafc643
Daniel <st161382@stud.uni-stuttgart.de>
2022-09-20 23:16:51 +0200

    first uniform variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
 import HelloWorld from "../components/HelloWorld.vue";
 import { onMounted } from "vue";
 import * as THREE from "three";
+import test from "node:test";
+import { Clock } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // Our Javascript will go here.
 
 onMounted(() => {
@@ -11,6 +14,9 @@ onMounted(() => {
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas
   });
+
+  
+  const clock = new THREE.Clock(true);
 
   renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -24,13 +30,28 @@ onMounted(() => {
   camera.position.z = 30;
   camera.position.x = 20;
 
+  const orbitcontrols = new OrbitControls(camera, canvas);
+  orbitcontrols.position0 = camera.position
+  
   const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16)
 
+  const uniformData = {
+    u_time: {
+      type: 'f',
+      value: clock.getElapsedTime()
+    }
+  }
   const boxMaterial = new THREE.ShaderMaterial({
     wireframe: true,
+    uniforms: uniformData,
     vertexShader: `
+    uniform float u_time;
     void main() {
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
+      
+      vec4 result;
+
+      result = vec4(position.x, sin(position.z + u_time) + position.y, position.z, 1.0);
+      gl_Position = projectionMatrix * modelViewMatrix * result;
     }`,
     fragmentShader: `
     void main() { gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); }`
@@ -40,6 +61,7 @@ onMounted(() => {
   scene.add(boxMesh)
   function animate() {
     requestAnimationFrame( animate );
+    uniformData.u_time.value = clock.getElapsedTime();
     /* cube.position.x = (cube.position.x + 0.01) % 10; 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01; */
