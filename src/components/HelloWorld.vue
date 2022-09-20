e3c36065d2d9689a97a7f7fd7ba50a25511ec82b
Daniel <st161382@stud.uni-stuttgart.de>
2022-09-20 22:02:34 +0200

    Vue3 + Three.js setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,13 +26,7 @@ const a = computed(() => {
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
-    </h3>
+    HEEE
   </div>
 </template>
 
@@ -45,6 +39,7 @@ h1 {
 
 h3 {
   font-size: 1.2rem;
+  color: white;
 }
 
 .greetings h1,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+import { onMounted } from "vue";
+import * as THREE from "three";
+// Our Javascript will go here.
+
+onMounted(() => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const canvas = document.getElementById("canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+  });
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+  }
+  animate();
+})
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <canvas id="canvas"> </canvas>
+    <HelloWorld msg = "ok" />
   </main>
 </template>
+
+<style scoped>
+  #canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
